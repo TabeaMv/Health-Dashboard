7bf7adfe99a8cff3486935182122b9e858ab3e52
TabeaMv <tabea.merlevede@gmail.com>
2025-09-27 14:48:44 +0200

    Start new function to get valid access token

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+from dotenv import load_dotenv
+import os
 
-with open("C:/Users/tabea/Desktop/Own Projects/FitbitDashboard/tokens.json") as f:
+
+### Get the first authorization code
+# load_dotenv()
+# client_id = os.getenv("CLIENT_ID")
+# code = "a13d12e15637a08254d042fe6fd9a350e4468434" # already used
+# client_secret = os.getenv("CLIENT_SECRET")
+# redirect_uri = os.getenv("REDIRECT_URI")
+# payload = {'code': code, 
+#            'redirect_uri': redirect_uri, 
+#            'client_id': client_id, 
+#            'grant_type': 'authorization_code'}
+# client_info = HTTPBasicAuth(client_id, client_secret)
+
+# r = requests.post('https://api.fitbit.com/oauth2/token', data=payload, auth=client_info)
+
+# print(r.text)
+
+
+def get_valid_access_token():
+    pass
+
+
+with open("tokens.json") as f:
     d = json.load(f)
+    access_token, refresh_token = d["access_token"], d["refresh_token"]
 
-client_id = "23TJQ9"
-code = "958a3de68656eae849a2921601bdb14634165af8"
-client_secret = "400476a08bb45734357899e5fb32ea8b"
-redirect_uri = "http://localhost:8080"
-payload = {'code': code, 
-           'redirect_uri': redirect_uri, 
-           'client_id': client_id, 
-           'grant_type': 'authorization_code'}
-client_info = HTTPBasicAuth(client_id, client_secret)
-
-r = requests.post('https://api.fitbit.com/oauth2/token', data=payload, auth=client_info)
-
-print(r.text)
 
 
 # from dotenv import load_dotenv
